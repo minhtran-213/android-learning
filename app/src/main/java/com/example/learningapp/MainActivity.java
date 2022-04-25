9bd0471dccf5007bce362f89ab642145d4861416
minhtranb <minhtranb@nashtechglobal.com>
2022-04-25 16:38:29 +0700

    Complete lesson implicit intent

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        switch (view.getId()){
+            case R.id.btn_browser:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+                startActivity(intent);
+                break;
+            case R.id.btn_dial:
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:+84967628624"));
+                startActivity(intent1);
+                break;
+            case R.id.btn_map:
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:20.5937,789629"));
+                startActivity(intent2);
+                break;
+        }
     }
 }
