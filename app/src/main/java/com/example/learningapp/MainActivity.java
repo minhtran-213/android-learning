3a225cb7ce0bc56af3abeba8e519cc0e6e167d34
minhtranb <minhtranb@nashtechglobal.com>
2022-04-26 14:26:42 +0700

    Complete lesson UI Controller

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button alertPopupBtn;
+    Button progressPopupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alertPopupBtn = (Button) findViewById(R.id.btn_alert_popup);
+        progressPopupBtn = (Button) findViewById(R.id.btn_progress_popup);
+
+
+        alertPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder myAlert = new AlertDialog.Builder(MainActivity.this);
+                myAlert.setTitle("Alert demo");
+                myAlert.setMessage("Are you sure?");
+                myAlert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                myAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                myAlert.show();
+            }
+        });
+
+        progressPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("Downloading");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setMessage("Please waiting for a moment");
+                progressDialog.setButton(ProgressDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                progressDialog.show();
+            }
+        });
     }
 }
